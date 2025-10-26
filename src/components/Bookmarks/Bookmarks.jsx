c7e9904b61c4591a8709fe0734c739b70833a407
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-10-26 19:31:07 +0600

    adding the display the time boc of reading speand time on the bookmars component

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,7 +1,12 @@
 const Bookmarks = () => {
   return (
-    <div className="border w-1/3">
-      <h2>I'm from Bookmarks</h2>
+    <div className="pt-2.5 w-1/3">
+      {/* reading time Box */}
+      <div className="py-5 bg-[#6047EC1A] border border-[#6047EC] rounded-lg">
+        <h3 className="text-[#6047EC] text-2xl font-bold text-center">
+          Spent time on read : 177 min
+        </h3>
+      </div>
     </div>
   );
 };
