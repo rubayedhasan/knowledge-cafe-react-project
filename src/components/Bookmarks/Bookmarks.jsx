61cb12e9adbd0bb54e2aca11e48423a6cc519aca
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-10-26 22:15:11 +0600

    adding bookmark items structure

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -7,6 +7,19 @@ const Bookmarks = () => {
           Spent time on read : 177 min
         </h3>
       </div>
+
+      {/* section:: bookmark list  */}
+      <div className="mt-6 p-7 rounded-lg bg-[#1111110D] min-h-40 lg:min-h-80">
+        <h3 className="font-bold text-2xl text-[#111111]">
+          Bookmarked Blogs : 8
+        </h3>
+        <div className="mt-4 space-y-4">
+          <p>Bookmark item</p>
+          <p>Bookmark item</p>
+          <p>Bookmark item</p>
+          <p>Bookmark item</p>
+        </div>
+      </div>
     </div>
   );
 };
