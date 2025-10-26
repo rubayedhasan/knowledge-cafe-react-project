e63ebacaa377eb93f5382f7ac93d28dc382eb540
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-10-26 16:43:32 +0600

    fetch the data on the blogs component

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Blogs = () => {
+  // the state -> blogs
+  const [blogs, setBlogs] = useState([]);
+
+  //   use effect-> fetch the data
+  useEffect(() => {
+    fetch("blogs-data.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className=" border w-2/3">
       <h2>I'm from blogs</h2>
